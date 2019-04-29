900fa032c5f1f3295dffc4bb40fa4512100c85e2
tsmith <tsmith@cartrawler.com>
2019-04-29 20:49:10 +0100

    Header multiple options

--- src/components/header.js
+++ src/components/header.js
@@ -5,28 +5,51 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `black`,
+      marginBottom: `1rem`,
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        display: `flex`,
+        margin: `0 auto`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div
+        style={{
+          padding: `1rem 1rem`,
+        }}
+      >
+        <h4 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h4>
+      </div>
+      <div
+        style={{
+          padding: `1rem 1rem`,
+        }}
+      >
+        <h4 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {"Contact"}
+          </Link>
+        </h4>
+      </div>
     </div>
   </header>
 )
